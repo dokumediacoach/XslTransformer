fd46f38e3a64fe7610e77cc8067794073ba3049f
Matthias Schleiff <info@dokumediacoach.de>
2017-08-19 18:09:12 +0200

    cleanup

--- XslTransformer.Standard/Model/XmlStylesheet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace XslTransformer.Core
-{
-    public class XmlStylesheet
-    {
-        public string Media { get; set; } = "all";
-        public string Href { get; set; } = null;
-        public bool IsSelected { get; set; } = true;
-    }
-}
